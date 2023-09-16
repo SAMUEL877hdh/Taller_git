e9cf4e66ebcc87873fb5a969ae0d3cccd0cc8175
SAMUEL877hdh <143855386+SAMUEL877hdh@users.noreply.github.com>
2023-09-15 21:23:26 -0500

    Update logicd.js

--- logicd.js
+++ logicd.js
@@ -1,2 +1,27 @@
-console.log("Hola mundo")
-console.log("black reaper")
+var numero;
+var resultado;
+
+numero = parseInt(prompt("Ingrese un número: "));
+
+ switch (true) {
+
+    case (numero > 0 && numero < 10):
+        resultado = numero * numero;
+        alert("El resultado es " + resultado);
+        break;
+
+    case (numero >= 10 && numero < 100):
+        resultado = numero * 2;
+        alert("El resultado es " + resultado);
+        break;
+
+    case (numero >= 100 && numero < 1000):
+        resultado = numero - 100;
+        alert("El resultado es " + resultado);
+        break;
+
+    default:
+        alert("Número no válido");
+        break;
+
+}
